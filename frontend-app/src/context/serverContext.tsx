23dc1d7dcebfff9990e1ddd8e407c20283b5129d
lawrient <sandincl.d@gmail.com>
2025-04-08 08:27:04 +0800

    handle serverContext

--- frontend-app/src/context/serverContext.tsx
+++ frontend-app/src/context/serverContext.tsx
@@ -1,3 +1,23 @@
-export interface Server{
-    
+export interface Server {
+  id: string;
+  name: string;
+  picture: string;
+  description: string;
+  is_private: boolean;
+  invite_code: string;
+  created_at: string;
+  updated_at: string;
+  room: RoomServer[];
+}
+
+export interface RoomServer {
+  id: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+interface ServerContextType {
+  servers: Server[];
+  setServers: (servers: Server[]) => void;
 }
