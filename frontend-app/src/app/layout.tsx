b07b5b9bbd640f636ef0dc5897a7bc007d47dd7a
lawrient <sandincl.d@gmail.com>
2025-05-24 23:52:10 +0800

    fialure

--- frontend-app/src/app/layout.tsx
+++ frontend-app/src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import DisableRightClick from "@/components/DisableRightClick";
+import { AuthProvider } from "@/context/authContext";
+import { ServerProvider } from "@/context/serverContext";
+import { FriendProvider } from "@/context/friendContext";
+import { getUser } from "@/get-data/getUser";
+import { redirect } from "next/navigation";
+import getServer from "@/get-data/getServer";
+import getFriend from "@/get-data/getFriend";
 
 export const metadata: Metadata = {
   title: "Clone Discord",
@@ -8,17 +15,29 @@ export const metadata: Metadata = {
   icons: "dcicon.png",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <DisableRightClick />
-        {children}
-      </body>
-    </html>
-  );
+  const user = await getUser();
+  if (!user) {
+    redirect("/login");
+  }
+  const servers = await getServer();
+  const friends = await getFriend();
+
+  if (servers)
+    return (
+      <html lang="en">
+        <body>
+          <DisableRightClick />
+          <AuthProvider get_user={user}>
+            <ServerProvider get_servers={servers}>
+              <FriendProvider get_friends={friends}>{children}</FriendProvider>
+            </ServerProvider>
+          </AuthProvider>
+        </body>
+      </html>
+    );
 }
