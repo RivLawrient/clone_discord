30d8314f26e893d5eedff2df9fb1551be25b2c1c
lawrient <sandincl.d@gmail.com>
2025-05-26 10:15:18 +0800

    route problem

--- frontend-app/src/app/(main)/channels/[server]/page.tsx
+++ frontend-app/src/app/(main)/channels/[server]/page.tsx
@@ -1,4 +1,29 @@
-"use client";
+import { Server, useServer } from "@/context/serverContext";
+import getServer from "@/get-data/getServer";
+
+export async function generateStaticParams() {
+  const servers: Server[] = await fetch(
+    `${process.env.HOST_API_PUBLIC}/api/my_server`,
+    {
+      method: "GET",
+      // headers: {
+      //   Cookie: `session=${cookie.get("session")?.value}`,
+      // },
+    },
+  ).then(async (res) => {
+    if (res.ok) {
+      const data = await res.json();
+      return data.data;
+    }
+    return [];
+  });
+
+  if (servers)
+    return servers.map((v) => ({
+      server: v.id,
+    }));
+}
+
 export default function Page() {
   return <>server</>;
 }
